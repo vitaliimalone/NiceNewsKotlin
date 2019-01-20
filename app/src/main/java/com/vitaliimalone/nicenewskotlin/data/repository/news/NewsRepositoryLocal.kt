cc70803f31c79ccc163fda24696bf4b9f29cff4c
Vitalii Malyi <vitaliimalone@gmail.com>
2019-01-20 15:55:39 +0200

    refactor: cleanup

--- app/src/main/java/com/vitaliimalone/nicenewskotlin/data/repository/news/NewsRepositoryLocal.kt
+++ app/src/main/java/com/vitaliimalone/nicenewskotlin/data/repository/news/NewsRepositoryLocal.kt
@@ -7,6 +7,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class NewsRepositoryLocal(private val newsDao: NewsDao) : BaseRepositoryLocal() {
+    // FIXME: workaround until Room 2.1.0-alpha04 is released
+    // FIXME: https://issuetracker.google.com/issues/120227284
     suspend fun storeNews(news: List<News>) {
         return withContext(Dispatchers.IO) {
             newsDao.deleteAll()
