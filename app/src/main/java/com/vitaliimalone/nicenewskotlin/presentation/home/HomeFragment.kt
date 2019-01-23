aa5d8d2a4b98f20c92539ef4a1cf84d66debf00b
Vitalii Malyi <vitaliimalone@gmail.com>
2019-01-23 08:06:01 +0200

    feat: add menu items

--- app/src/main/java/com/vitaliimalone/nicenewskotlin/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/vitaliimalone/nicenewskotlin/presentation/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import com.vitaliimalone.nicenewskotlin.R
 import com.vitaliimalone.nicenewskotlin.presentation.common.BaseFragment
 import com.vitaliimalone.nicenewskotlin.presentation.home.common.HomePagerAdapter
+import com.vitaliimalone.nicenewskotlin.utils.toast
 import kotlinx.android.synthetic.main.home_fragment.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -16,7 +17,20 @@ class HomeFragment : BaseFragment() {
         super.onActivityCreated(savedInstanceState)
         viewPager.adapter = HomePagerAdapter(requireFragmentManager())
         tabLayout.setupWithViewPager(viewPager)
-
+        bottomAppBar.replaceMenu(R.menu.home_menu)
+        bottomAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.menu_favorites -> {
+                    toast("Favorites")
+                    true
+                }
+                R.id.menu_settings -> {
+                    toast("Settings")
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     companion object {
